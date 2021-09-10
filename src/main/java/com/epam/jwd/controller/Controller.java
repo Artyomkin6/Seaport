4efb384acee21d396bc3bf986006524d367cdba0
Artyom <artyomkin6@gmail.com>
2021-09-10 17:39:07 +0300

    Init commit

---
+++ src/main/java/com/epam/jwd/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.epam.jwd.controller;
+
+public class Controller {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.run();
+    }
+
+    public void run() {
+
+    }
+}
